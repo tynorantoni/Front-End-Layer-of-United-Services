3780b597c4f375cb3ef5f5d295fc28294e2e3e68
pawel <pawel.szymaszek@gmail.com>
2020-12-08 13:59:56 +0100

    enabling CI, CD heroku travis and docker

---
+++ czarnaFrotte/Dockerfile
@@ -0,0 +1,14 @@
+FROM python:3
+
+WORKDIR /usr/src/app
+
+ADD requirements.txt /usr/src/app
+
+RUN pip install -r requirements.txt
+
+ADD . /usr/src/app
+
+# collect static files
+RUN python manage.py collectstatic --noinput
+
+CMD gunicorn airline.wsgi:application --bind 0.0.0.0:$PORT
--- czarnaFrotte/czarnaFrotte/settings.py
+++ czarnaFrotte/czarnaFrotte/settings.py
@@ -76,10 +76,23 @@ WSGI_APPLICATION = 'czarnaFrotte.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'postgres',
+
+        'USER': 'posgrusr',
+
+        'PASSWORD': 'ZF?v+xn3zeuRS+^f',
+
+        'HOST': 'bike-portal-db.cn5z7dnfwfmr.us-east-2.rds.amazonaws.com',
+
+        'PORT': '5432',
+
     }
+
 }
 
 
