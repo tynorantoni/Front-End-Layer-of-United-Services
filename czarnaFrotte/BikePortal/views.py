811508ba6a9d1049312da17a8e62de0763f95679
pawel <pawel.szymaszek@gmail.com>
2020-12-08 23:00:25 +0100

    enabling CI, CD heroku travis and docker todo - templates, views, models, tests?

--- czarnaFrotte/BikePortal/views.py
+++ czarnaFrotte/BikePortal/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request,'index.html',context={})
+
+def cycling_stats(request):
+    pass
+
+def cycling_data(request):
+    pass
+
+def maintenance(request):
+    pass
+
+
