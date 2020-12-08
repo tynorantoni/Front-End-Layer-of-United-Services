c0b1b5f95c00213e57367cb14879c8643819cd2e
pawel <pawel.szymaszek@gmail.com>
2020-12-08 10:07:30 +0100

    tailwind struggles

--- czarnaFrotte/czarnaFrotte/settings.py
+++ czarnaFrotte/czarnaFrotte/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TAILWIND_APP_NAME = 'theme'
+NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
---
+++ czarnaFrotte/was/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WasConfig(AppConfig):
+    name = 'was'
