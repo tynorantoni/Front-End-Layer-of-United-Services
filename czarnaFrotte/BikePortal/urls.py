4d86d04317116aa9127b63e4c1d62bc1203c3782
pawel <pawel.szymaszek@gmail.com>
2020-12-10 08:12:23 +0100

    working with templates
    todo CI, CD heroku travis and docker

--- czarnaFrotte/BikePortal/urls.py
+++ czarnaFrotte/BikePortal/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('cycling-stats', views.cycling_stats, name='cycling_stats'),
+    path('cycling-data', views.cycling_data, name='cycling_data'),
+    path('maintenance', views.maintenance, name='maintenance'),
 ]
--- czarnaFrotte/BikePortal/views.py
+++ czarnaFrotte/BikePortal/views.py
@@ -2,15 +2,23 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'index.html',context={})
+    alerts={}
+    context={'alerts':alerts}
+    return render(request,'index.html',context)
 
 def cycling_stats(request):
-    pass
+    alerts = {}
+    context = {'alerts': alerts}
+    return render(request, 'cycling-stats.html', context)
 
 def cycling_data(request):
-    pass
+    alerts = {}
+    context = {'alerts': alerts}
+    return render(request, 'cycling-data.html', context)
 
 def maintenance(request):
-    pass
+    alerts = {}
+    context = {'alerts': alerts}
+    return render(request, 'maintenance.html', context)
 
 
